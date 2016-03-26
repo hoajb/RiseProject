49b15d6bac5c71f98c3c66ead74751020f38b522
Hoa Nguyen <hoa.nguyenminh.it@gmail.com>
2016-03-26 23:09:05 +0700

    Viewpager Loader image + Universal Image Loader

--- app/src/main/java/vn/com/rise/project/activity/ImageGalleryViewActivity.java
+++ app/src/main/java/vn/com/rise/project/activity/ImageGalleryViewActivity.java
@@ -1,29 +1,42 @@
 package vn.com.rise.project.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-import com.veinhorn.scrollgalleryview.MediaInfo;
-import com.veinhorn.scrollgalleryview.ScrollGalleryView;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import vn.com.rise.project.R;
 import vn.com.rise.project.Utils.LoadingDataAsyncTask;
-import vn.com.rise.project.Utils.PicassoImageLoader;
 
 /**
  * Created by hoanguyen on 3/22/16.
  */
 public class ImageGalleryViewActivity extends BaseActivity {
 
-    private ScrollGalleryView scrollGalleryView;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_image_gallery_view);
+        setContentView(R.layout.activity_image_gallery_view_2);
         initView();
         getData();
     }
@@ -38,41 +51,109 @@ public class ImageGalleryViewActivity extends BaseActivity {
         new LoadingDataAsyncTask(this, ViewListData.TYPE_IMAGE, mPos, new LoadingDataAsyncTask.PostExecuteListener() {
             @Override
             public void onPostExecuteListener(List<String> result) {
+                mViewPager.setAdapter(new ImageAdapter(ImageGalleryViewActivity.this, result));
 
-                List<MediaInfo> infos = new ArrayList<>(result.size());
-                for (String url : result)
-                    infos.add(MediaInfo.mediaLoader(new PicassoImageLoader(url)));
-
-                scrollGalleryView
-                        .setThumbnailSize(100)
-                        .setZoom(true)
-                        .setFragmentManager(getSupportFragmentManager())
-//                .addMedia(MediaInfo.mediaLoader(new DefaultImageLoader(R.drawable.wallpaper1)))
-//                .addMedia(MediaInfo.mediaLoader(new DefaultImageLoader(toBitmap(R.drawable.wallpaper7))))
-//                .addMedia(MediaInfo.mediaLoader(new MediaLoader() {
-//                    @Override public boolean isImage() {
-//                        return true;
-//                    }
-//
-//                    @Override public void loadMedia(Context context, ImageView imageView,
-//                                                    MediaLoader.SuccessCallback callback) {
-//                        imageView.setImageBitmap(toBitmap(R.drawable.wallpaper3));
-//                        callback.onSuccess();
-//                    }
-//
-//                    @Override public void loadThumbnail(Context context, ImageView thumbnailView,
-//                                                        MediaLoader.SuccessCallback callback) {
-//                        thumbnailView.setImageBitmap(toBitmap(R.drawable.wallpaper3));
-//                        callback.onSuccess();
-//                    }
-//                }))
-//                .addMedia(MediaInfo.mediaLoader(new DefaultVideoLoader(movieUrl, R.mipmap.default_video)))
-                        .addMedia(infos);
             }
         }).execute();
     }
 
     private void initView() {
-        scrollGalleryView = (ScrollGalleryView) findViewById(R.id.scroll_gallery_view);
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        mViewPager.setOffscreenPageLimit(3);
+    }
+
+    private static class ImageAdapter extends PagerAdapter {
+
+        private List<String> IMAGE_URLS;
+
+        private LayoutInflater inflater;
+        private DisplayImageOptions options;
+
+        ImageAdapter(Context context, List<String> listPath) {
+            IMAGE_URLS = listPath;
+            inflater = LayoutInflater.from(context);
+
+            options = new DisplayImageOptions.Builder()
+                    .showImageForEmptyUri(R.drawable.ic_failed)
+                    .showImageOnFail(R.drawable.ic_failed)
+                    .resetViewBeforeLoading(true)
+                    .imageScaleType(ImageScaleType.EXACTLY)
+                    .bitmapConfig(Bitmap.Config.RGB_565)
+                    .considerExifParams(true)
+                    .displayer(new FadeInBitmapDisplayer(300))
+                    .build();
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+        @Override
+        public int getCount() {
+            return IMAGE_URLS.size();
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup view, int position) {
+            View imageLayout = inflater.inflate(R.layout.item_pager_image, view, false);
+            assert imageLayout != null;
+            ImageView imageView = (ImageView) imageLayout.findViewById(R.id.image);
+            final ProgressBar spinner = (ProgressBar) imageLayout.findViewById(R.id.loading);
+
+            ImageLoader.getInstance().displayImage("file://" + IMAGE_URLS.get(position), imageView, options, new SimpleImageLoadingListener() {
+                @Override
+                public void onLoadingStarted(String imageUri, View view) {
+                    spinner.setVisibility(View.VISIBLE);
+                }
+
+                @Override
+                public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                    String message = null;
+                    switch (failReason.getType()) {
+                        case IO_ERROR:
+                            message = "Input/Output error";
+                            break;
+                        case DECODING_ERROR:
+                            message = "Image can't be decoded";
+                            break;
+                        case NETWORK_DENIED:
+                            message = "Downloads are denied";
+                            break;
+                        case OUT_OF_MEMORY:
+                            message = "Out Of Memory error";
+                            break;
+                        case UNKNOWN:
+                            message = "Unknown error";
+                            break;
+                    }
+                    Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
+
+                    spinner.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                    spinner.setVisibility(View.GONE);
+                }
+            });
+
+            view.addView(imageLayout, 0);
+            return imageLayout;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view.equals(object);
+        }
+
+        @Override
+        public void restoreState(Parcelable state, ClassLoader loader) {
+        }
+
+        @Override
+        public Parcelable saveState() {
+            return null;
+        }
     }
 }
---
+++ app/src/main/java/vn/com/rise/project/activity/RiseApplication.java
@@ -0,0 +1,35 @@
+package vn.com.rise.project.activity;
+
+import android.app.Application;
+
+import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+
+/**
+ * Created by hoanguyen on 3/26/16.
+ */
+public class RiseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // UNIVERSAL IMAGE LOADER SETUP
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .cacheOnDisk(true).cacheInMemory(true)
+                .imageScaleType(ImageScaleType.EXACTLY)
+                .displayer(new FadeInBitmapDisplayer(300)).build();
+
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
+                getApplicationContext())
+//                .defaultDisplayImageOptions(defaultOptions)
+                .memoryCache(new WeakMemoryCache())
+                .diskCacheSize(100 * 1024 * 1024).build();
+
+        ImageLoader.getInstance().init(config);
+//        // END - UNIVERSAL IMAGE LOADER SETUP
+    }
+}
